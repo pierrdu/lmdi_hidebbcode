3bdfd878209b8d9fff046af1349995cb1ac8cebc
Pierre <pierre@duhem.com>
2021-10-22 08:09:09 +0200

    1.0.4

--- event/listener.php
+++ event/listener.php
@@ -1,8 +1,8 @@
 <?php
 /**
 *
-* @package phpBB Extension - LMDI Delete Re:
-* @copyright (c) 2015 LMDI - Pierre Duhem
+* @package phpBB Extension - LMDI Hide BB Code
+* @copyright (c) 2015-2021 LMDI - Pierre Duhem
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
@@ -16,6 +16,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
 * Event listener
+* L'événement se tropuve dans includes/functions_display.php
 */
 class listener implements EventSubscriberInterface
 {
@@ -23,7 +24,6 @@ class listener implements EventSubscriberInterface
 	static public function getSubscribedEvents ()
 	{
 	return array(
-		// 'core.display_custom_bbcodes_modify_row'	=> 'hide_bbcode',
 		'core.display_custom_bbcodes_modify_sql'	=> 'hide_bbcode',
 	);
 	}
@@ -33,33 +33,46 @@ class listener implements EventSubscriberInterface
 
 	/** @var \phpbb\auth\auth */
 	protected $auth;
+	protected $db;
 
 	/**
 	* Constructor
 	*
+	* @param \phpbb\controller\helper	$helper	Controller helper object
+	* @param \phpbb\template			$template	Template object
 	*/
-	public function __construct(\phpbb\user $user, \phpbb\auth\auth $auth)
+	public function __construct(\phpbb\user $user,
+		\phpbb\auth\auth $auth,
+		\phpbb\db\driver\driver_interface $db
+		)
 	{
 		$this->user = $user;
 		$this->auth = $auth;
+		$this->db = $db;
 	}
 
 	public function hide_bbcode ($event)
 	{
-		global $request;
-		$fid = $request->variable ('f', 0);
+		$fid = request_var ('f', 0);
+		if (!$fid)
+		{
+			$tid = request_var ('t', 0);
+			$sql = 'SELECT forum_id
+			FROM ' . TOPICS_TABLE . "
+			WHERE topic_id = $tid";
+			$result = $this->db->sql_query($sql);
+			$fid = (int) $this->db->sql_fetchfield('forum_id');
+			$this->db->sql_freeresult($result);
+		}
 		$sql_ary = $event['sql_ary'];
 		$auto = 0;
 		$autom  = $this->auth->acl_get('m_', $fid);
 		$autoa  = $this->auth->acl_get('a_', $fid);
 		if ($autom)
-		{
 			$auto = 1;
-		}
 		if ($autoa)
-		{
 			$auto = 2;
-		}
+		// var_dump ($auto);
 		switch ($auto)
 		{
 			case 0 :
@@ -76,3 +89,4 @@ class listener implements EventSubscriberInterface
 	}
 
 }
+
